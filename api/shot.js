7862a48507cdef2af7e0692f187ced7cadee6ad9
Pterobyte <jt1992@gmail.com>
2019-08-24 16:48:41 +0800

    fix: disable sandbox

--- api/shot.js
+++ api/shot.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer')
 
 async function shot(url, type) {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
   const page = await browser.newPage()
   await page.goto(url)
   const file = await page.screenshot({ type })
